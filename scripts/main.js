1a598f7eed2d18a932e51133a828b34e477912b3
Net <annette@lemonade.nyc>
2023-09-28 18:51:15 -0500

    MainJS: update file path

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,9 @@
 // scripts/main.js 
 
-import "/scripts/header.js";
-import "/scripts/footer.js";
+import "../scripts/header.js";
+import "../scripts/footer.js";
 
-import "/scripts/a11y.js";
+import "../scripts/a11y.js";
 
-import "/scripts/slideshow.js";
-import "/scripts/portfolio.js";
+import "../scripts/slideshow.js";
+import "../scripts/portfolio.js";
