389441de6a8aab703dca02629b9aadc219e33f32
Net <annette@lemonade.nyc>
2023-10-19 12:24:13 -0500

    SiteSpeed: updates

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp'); 
-const minify = require('gulp-minify');
- 
-gulp.task('default', function() {
-  gulp.src(['scripts/*.js', 'scripts/*.mjs'])
-    .pipe(minify())
-    .pipe(gulp.dest('scripts'))
-});
--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,9 @@
 // scripts/main.js 
 
-import "../scripts/header-min.js";
-import "../scripts/footer-min.js";
+import "../scripts/header.min.js";
+import "../scripts/footer.min.js";
 
-import "../scripts/a11y-min.js";
+import "../scripts/a11y.min.js";
 
-import "../scripts/slideshow-min.js";
-import "../scripts/portfolio-min.js";
+import "../scripts/slideshow.min.js";
+import "../scripts/portfolio.min.js";
